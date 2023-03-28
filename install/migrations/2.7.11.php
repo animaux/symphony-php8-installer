8464f981b209b97767132ca30d16ba948e22f81f
animaux <ar@animaux.de>
2023-03-28 23:20:04 +0200

    initial commit 2.7.11

---
+++ install/migrations/2.7.11.php
@@ -0,0 +1,14 @@
+<?php
+
+    class migration_2711 extends Migration
+    {
+        public static function getVersion()
+        {
+            return '2.7.11';
+        }
+
+        public static function getReleaseNotes()
+        {
+            return 'https://www.getsymphony.com/download/releases/version/2.7.11/';
+        }
+    }
